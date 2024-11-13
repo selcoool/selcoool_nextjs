812b7ebb5ca1fd452088a53fbabde237c48b17db
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-11-14 03:48:33 +0800

    sds

--- src/app/(root)/home/page.tsx
+++ src/app/(root)/home/page.tsx
@@ -135,7 +135,6 @@ useEffect(()=>{
 
     return (
         <div className='bg-slate-200'>
-<h1>HHHHHHH</h1>
           {/* <div className="flex justify-center items-center ">
           <div>
       <button onClick={handleLogin}>Sign In</button>
